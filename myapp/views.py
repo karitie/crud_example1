21403fc88d7aa8c3f645855171a0aa81fbe04e31
karitie <karitiesamuel7@gmail.com>
2025-11-29 23:51:54 +0300

    Initial commit

---
+++ myapp/views.py
@@ -0,0 +1,44 @@
+from django.shortcuts import render,redirect
+from .models import User
+from .forms import UserForm
+# Create your views here.
+
+def user_list(request):
+    records=User.objects.all()
+    mydict={'records':records}
+    return render(request,'listingpage.html',context=mydict)
+
+def AddUser(request):
+    mydict={}
+    form=UserForm(request.POST or None , request.FILES or None)
+    if form.is_valid():
+        form.save()
+        return redirect('/')
+
+    mydict['form']=form
+    return render(request,'add.html',mydict)
+
+def EditUser(request,id=None):
+    one_rec=User.objects.get(pk=id)
+    form=UserForm(request.POST or None,request.FILES or None, instance=one_rec)
+    if form.is_valid():
+        form.save()
+        return redirect('/')
+    mydict= {'form':form}
+    return render(request,'edit.html',context=mydict)
+
+def DeleteUser(request,eid=None):
+    one_rec = User.objects.get(pk=eid)
+    if  request.method=="POST":
+         one_rec.delete()
+         return redirect('/')
+    return render(request,'delete.html')
+
+def ViewUser(request,eid=None):
+    mydict={}
+    one_rec = User.objects.get(pk=eid)
+    mydict['user']=one_rec
+    return render(request,'view.html',mydict)
+
+
+
